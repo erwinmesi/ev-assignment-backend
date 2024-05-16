a8ccc4f819b292283681dad1ce428e34a91b0992
Erwin Mesias <mesiaserwin@gmail.com>
2024-05-16 08:57:53 +0800

    feat(roles): add more default roles

--- config/roles.php
+++ config/roles.php
@@ -8,9 +8,24 @@ return [
             'description' => 'The user with the full access to the system.',
         ],
 
-        'admin' => [
-            'name' => 'admin',
+        'administrator' => [
+            'name' => 'administrator',
             'description' => 'A user with permissions to manage majority of the system except another admin.',
+        ],
+
+        'author' => [
+            'name' => 'author',
+            'description' => 'A user with permissions to write and manage their own posts.',
+        ],
+
+        'editor' => [
+            'name' => 'editor',
+            'description' => 'A user with permissions to write and manage their own posts and others.',
+        ],
+
+        'subscriber' => [
+            'name' => 'subscriber',
+            'description' => 'A user with permissions to read and comment on posts.',
         ]
     ],
 ];
